bc6eb86960d8855039901905735d702132611e47
Zayd <Zayd@Harrenhal>
2014-08-31 14:19:48 -0700

    Uploading a tester file for windows for assignment #1 as well as an additional test file.

---
+++ Assignment #1/src/Assignment #1 Tester.bat
@@ -0,0 +1,9 @@
+python food_agent.py test.txt manhattan
+python food_agent.py test.txt euclidean
+python food_agent.py test2.txt manhattan
+python food_agent.py test2.txt euclidean
+python food_agent.py test3.txt manhattan
+python food_agent.py test3.txt euclidean
+python food_agent.py test4.txt manhattan
+python food_agent.py test4.txt euclidean
+pause

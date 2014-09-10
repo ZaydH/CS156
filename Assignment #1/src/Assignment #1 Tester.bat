88c57fb667701546fcbe7d8e4235172896b97fa7
Zayd <Zayd@Harrenhal>
2014-09-10 02:32:50 -0700

    Adding made-up to Assignment #1 tester batch file.

--- Assignment #1/src/Assignment #1 Tester.bat
+++ Assignment #1/src/Assignment #1 Tester.bat
@@ -1,9 +1,13 @@
 python food_agent.py test.txt manhattan
 python food_agent.py test.txt euclidean
+python food_agent.py test.txt made_up
 python food_agent.py test2.txt manhattan
 python food_agent.py test2.txt euclidean
+python food_agent.py test2.txt made_up
 python food_agent.py test3.txt manhattan
 python food_agent.py test3.txt euclidean
+python food_agent.py test3.txt made_up
 python food_agent.py test4.txt manhattan
 python food_agent.py test4.txt euclidean
+python food_agent.py test4.txt made_up
 pause

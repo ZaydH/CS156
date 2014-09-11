37494e9ce765c634aa79621535d56550fabac4a0
Zayd <Zayd@Harrenhal>
2014-09-11 01:18:36 -0700

    Adding two more mazes one with no mouse and one with no goal.

--- Assignment #1/src/Assignment #1 Tester.bat
+++ Assignment #1/src/Assignment #1 Tester.bat
@@ -4,10 +4,19 @@ python food_agent.py test.txt made_up
 python food_agent.py test2.txt manhattan
 python food_agent.py test2.txt euclidean
 python food_agent.py test2.txt made_up
+pause
+python food_agent.py test2.txt made_up
 python food_agent.py test3.txt manhattan
 python food_agent.py test3.txt euclidean
 python food_agent.py test3.txt made_up
 python food_agent.py test4.txt manhattan
 python food_agent.py test4.txt euclidean
 python food_agent.py test4.txt made_up
+pause
+python food_agent.py "No Mouse.txt" manhattan
+python food_agent.py "No Mouse.txt" euclidean
+python food_agent.py "No Mouse.txt" made_up
+python food_agent.py "No Goal.txt" manhattan
+python food_agent.py "No Goal.txt" euclidean
+python food_agent.py "No Goal.txt" made_up
 pause

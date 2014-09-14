26141990f40f1e4fa9d9ff861cac8f431f5437d7
Zayd <Zayd@Harrenhal>
2014-09-13 19:12:51 -0700

    Cleaning tester file to move all the tester code that returns no result to the end of the file.

--- Assignment #1/src/Assignment #1 Tester.bat
+++ Assignment #1/src/Assignment #1 Tester.bat
@@ -13,9 +13,6 @@ python food_agent.py test2.txt manhattan
 python food_agent.py test2.txt euclidean
 python food_agent.py test2.txt made_up
 pause
-python food_agent.py test3.txt manhattan
-python food_agent.py test3.txt euclidean
-python food_agent.py test3.txt made_up
 python food_agent.py test4.txt manhattan
 python food_agent.py test4.txt euclidean
 python food_agent.py test4.txt made_up
@@ -24,6 +21,9 @@ python food_agent.py test5.txt manhattan
 python food_agent.py test5.txt euclidean
 python food_agent.py test5.txt made_up
 pause
+python food_agent.py test3.txt manhattan
+python food_agent.py test3.txt euclidean
+python food_agent.py test3.txt made_up
 python food_agent.py "No Mouse.txt" manhattan
 python food_agent.py "No Mouse.txt" euclidean
 python food_agent.py "No Mouse.txt" made_up

e3f1e3806aaea58575c131224b1ca5fd484db0af
Zayd <Zayd@Harrenhal>
2014-09-26 02:15:16 -0700

    Beginning some work on the initial structure of the driver code.  Also working on code to check for blocked moves.

--- Assignment #2/src/crazy_eights.py
+++ Assignment #2/src/crazy_eights.py
@@ -10,10 +10,9 @@ Team Member #2: Muffins Hammoudeh
 '''
 
 import random
-from enum import Enum
 
 
-class MoveType(Enum):
+class MoveType:
     '''
     '''
     normal_move = 1
@@ -31,7 +30,11 @@ class CrazyEight:
 
     def move(self, partial_state):
         '''
-        :param tuple partial_state: Tuple in the form of (face_up_card, suit, hand, history)
+        :param tuple partial_state: Tuple in the form:
+            (face_up_card, suit, hand, history)
+
+        :returns: A move in the form:
+            (player_num, face_up_card, suit, number_of_cards)
         '''
         # Extract the input parameters from the tuple.
         face_up_card = partial_state[0]
@@ -39,9 +42,16 @@ class CrazyEight:
         hand = partial_state[2]
         history = partial_state[3]
 
+        # Check if my current move is blocked.  If so, return immediately.
+        (move_type, next_move) = check_for_blocked_turn(history)
+        if(move_type != MoveType.normal_move):
+            return next_move
+
+        #  Determine available cards in deck and other player's hand.
+
     def move_perfect_knowledge(self, state):
         '''
-        :param tuple state: Tuple in the form of (deck, other_hand, partial_state)
+        :param tuple state: Tuple in the form (deck, other_hand, partial_state)
         '''
         # Extract the deck.
         deck = state[0]
@@ -54,6 +64,11 @@ class CrazyEight:
         suit = state[2][1]
         hand = state[2][2]
         history = state[2][3]
+
+        # Check if my current move is blocked.  If so, return immediately.
+        (move_type, next_move) = check_for_blocked_turn(history)
+        if(move_type != MoveType.normal_move):
+            return next_move
 
 
 def determine_card_rank(card_number):
@@ -79,13 +94,42 @@ def determine_card_suit(card_number):
     return card_number // 4
 
 
-def check_for_blocked_turn(history, ):
+def check_for_blocked_turn(history):
+    '''
+    This function checks if a move by the current player is blocked due to
+    a move made by the other player.
+
+    :param tuple history:
+    :returns: Tuple in the form of (MoveType, NextMove (if applicable)).
+            A NextMove is itself a tuple in the form:
+            (player_num, face_up_card, suit, number_of_cards)
+    '''
     # On the first move, regardless of face card, always a normal move.
     if(len(history) == 1):
-        return MoveType.normal_move
+        return (MoveType.normal_move, (-1, -1, -1, -1))
 
     #  Get the last move.
     last_move = history[len(history) - 1]
+
+    #  Parse the last move.
+    last_player = last_move[0]  # The player who played last.
+    last_discard = last_move[1]  # Face up card.
+    last_suit = last_move[2]
+    numb_picked_up_cards = last_move[3]  # Numb of cards picked up in last turn
+
+    # If on the last move someone drew, then this turn is always a normal move.
+    if(numb_picked_up_cards > 0):
+        return (MoveType.normal_move, (-1, -1, -1, -1))
+
+    # Check if the last move was queen of spades.
+    if(last_player != 0 and last_discard == MoveType.queen_of_spades):
+        # On a queen of spades, must draw five cards.
+        return (MoveType.queen_of_spades, (0, last_discard, last_suit, 5))
+
+    # Check if the last move was a jack
+    if(last_player != 0 and
+       determine_card_rank(last_discard) == MoveType.jack):
+        return (MoveType.jack, (0, last_discard, last_suit, 1))
 
 
 def parse_played_history(history):
@@ -123,9 +167,6 @@ def parse_played_history(history):
         # Return the play history.
         return (numb_cards_per_player[0], numb_cards_per_player[1],
                 discarded_cards)
-
-
-def
 
 
 def shuffle_deck(valid_cards):
--- Assignment #2/src/driver.py
+++ Assignment #2/src/driver.py
@@ -8,3 +8,44 @@ Team Member #2: Muffins Hammoudeh
         (No student ID - she's my cat but was here with me
         while I worked so she deserves credit).
 '''
+
+
+def build_initial_deck():
+    '''
+    Builds the initial deck of cards.
+
+    :returns: List of 52 cards shuffled.
+    '''
+    # Initialize the array
+    temp_deck = [None] * cards_per_deck
+
+    # Load the deck
+    for i in range(0, cards_per_deck):
+        temp_deck[i] = i
+
+    # Shuffle the temporary deck
+    shuffle_deck(temp_deck)
+
+    # Return the temporary deck
+    return temp_deck
+
+
+# import everything from the crazy eights file.
+from crazy_eights import *
+
+
+#  Define lists used for printing later.
+card_rank_names = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+                   "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+suit_names = ("Spades", "Hearts", "Diamonds", "Clubs")
+cards_per_deck = 52
+
+
+# Define empty array for the starting hands.
+human_player_hand = []
+computer_player_hand = []
+
+print "Welcome to the Wild, Weird, and Funky World of Crazy Eights."
+
+print build_initial_deck()
+

95d62a2edcf95d833d04d0cc14b343ac04feb311
Zayd <Zayd@Harrenhal>
2014-10-26 00:44:41 -0700

    Uploading code for HW3_Q1_SympySolver to Git to run on the Mac.

--- Assignment #3/src/HW3_Q1_Sympy_Solver.py
+++ Assignment #3/src/HW3_Q1_Sympy_Solver.py
@@ -9,6 +9,9 @@ from sympy.core.symbol import Symbol
 from UserString import MutableString
 
 
+global file_out
+
+
 def pretty_print_CNF(input_string):
     output_str = MutableString()
     # Remove nots and white space.
@@ -39,22 +42,27 @@ def pretty_print_CNF(input_string):
 
 
 def calculate_minimax(V, output, is_min):
+
+    # Determine the function for min/max
+    if(is_min):
+        op = And
+    else:
+        op = Or
+
     for b in xrange(0, 3):
         # Set the starting value of the function
         bool_func = is_min
-        if(is_min):
-            op = And
-        else:
-            op = Or
         # Iterate through all tree children.
         for i in xrange(0, 3):
             # Set the starting value of the clause
             clause = True
             # Concatenate previous bits.
             for prev_b in xrange(0, b):
+                # Determine bit equivalence
+                equiv = Or(And(output[prev_b], V[i][prev_b]),
+                           And(Not(output[prev_b]), Not(V[i][prev_b])))
                 # Build the previous bits
-                clause = And(clause,
-                             Equivalent(V[prev_b], V[i][prev_b]))
+                clause = And(clause, equiv)
             # Build the clause
             clause = And(clause, V[i][b])
             # Append this clause
@@ -63,6 +71,17 @@ def calculate_minimax(V, output, is_min):
         output.append(bool_func)
 
 
+def file_and_console_print(string, include_new_line=True):
+    if(include_new_line):
+        print string
+        file_out.write(string + "\n")
+    else:
+        print string,
+        file_out.write(string)
+
+file_out = open("HW3_Q1_Results.txt", "w")
+
+file_and_console_print("Defining Base Variables...", False)
 # Base Variables
 V = []
 # Create the array of symbols and concatenate a dummy placeholder.
@@ -80,29 +99,40 @@ for i in xrange(0, 3):
                            + "," + str(k+1) + "," \
                            + str(m+1) + "," + str(b+1)
                     V[i][j][k][m].append(Symbol(name))
+file_and_console_print("Done")
 
-
-output = []
-for b in xrange(0, 3):
-    level1 = []
-    for i in xrange(0, 3):
-        level1.append([])
-        level2 = []
-        for j in xrange(0, 3):
-            level2.append([])
-            level3 = []
-            for k in xrange(0, 3):
-                level3.append([])
-                # Perform the bit manipulations one level up.
-                is_min = True
-                calculate_minimax(V[i][j][k], level3[k], is_min)
-            # Perform the bit manipulations at the third level.
-            is_min = not is_min
-            calculate_minimax(level3[j], level2[j], is_min)
-        # Perform the bit manipulations at the second level.
+file_and_console_print("\n\n\nBuilding Minimax Dependencies...", False)
+# Build the tree of related variables.
+level1 = []
+for i in xrange(0, 3):
+    level1.append([])
+    level2 = []
+    for j in xrange(0, 3):
+        level2.append([])
+        level3 = []
+        for k in xrange(0, 3):
+            level3.append([])
+            # Perform the bit manipulations one level up.
+            is_min = True
+            calculate_minimax(V[i][j][k], level3[k], is_min)
+        # Perform the bit manipulations at the third level.
         is_min = not is_min
-        calculate_minimax(level2[i], level1[i], is_min)
-    # Perform the bit manipulations at the top level.
+        calculate_minimax(level3, level2[j], is_min)
+    # Perform the bit manipulations at the second level.
     is_min = not is_min
-    calculate_minimax(level1, Output, is_min)
+    calculate_minimax(level2, level1[i], is_min)
 
+# Perform the bit manipulations at the top level.
+is_min = not is_min
+output = []
+calculate_minimax(level1, output, is_min)
+file_and_console_print("Done")
+
+file_and_console_print("\n\n\n\nConverting to CNF...")
+# Print the output
+for b in xrange(0, 3):
+    cnf_string = pretty_print(to_cnf(output[b]))
+    file_and_console_print("\n\n\n\n\nV%d=" % (b))
+    file_and_console_print(cnf_string)
+
+file_out.close()

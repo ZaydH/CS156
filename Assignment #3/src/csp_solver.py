70d23267d8e17b254e2d60dad3bc89e52906afac
Zayd <Zayd@Harrenhal>
2014-10-29 07:32:15 -0700

    Adding my name and student ID information to the homework code. Uploading latest version of the written problems.

--- Assignment #3/src/csp_solver.py
+++ Assignment #3/src/csp_solver.py
@@ -1,7 +1,12 @@
 '''
 Created on Oct 18, 2014
 
-@author: Zayd
+@author: Zayd Hammoudeh
+
+Team Member #1: Zayd Hammoudeh (009418877)
+Team Member #2: Muffins Hammoudeh
+        (No student ID - she's my cat but was here with me
+        while I worked so she deserves credit).
 '''
 
 import string
